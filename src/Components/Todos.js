f8185c42ce44327a97e7bc0dd9fb0939434f2c70
jayrexacilo <acilojayrex@gmail.com>
2017-11-20 22:44:40 +0800

    Added inline edting for items and removed edit button

--- src/Components/Todos.js
+++ src/Components/Todos.js
@@ -1,48 +1,67 @@
-import React from "react";
+import React, { Component } from "react";
+import InlineEdit from "react-edit-inplace";
 
-const Todos = props => {
-  let todoItems;
-  if (props.todos) {
-    todoItems = props.todos.map((todo, i) => {
-      return (
-        <li
-          key={i}
-          className="list-group-item list-group-item-action text-left"
-        >
-          {todo.todoItem}
-          <div className="btn-toolbar float-right">
-            <button className="btn btn-light edit" title="Edit">
-              <i className="fa fa-pencil-square-o" aria-hidden="true" />
-            </button>
-            <button
-              className="btn btn-success done"
-              title="Done"
-              onClick={props.onMoveItem.bind(this, todo, "todos")}
-            >
-              <i className="fa fa-check-square-o" aria-hidden="true" />
-            </button>
-            <button
-              className="btn btn-danger delete"
-              title="Delete"
-              onClick={props.onDelete.bind(this, todo.id, "todos")}
-            >
-              <i className="fa fa-trash-o" aria-hidden="true" />
-            </button>
-          </div>
-        </li>
-      );
-    });
+class Todos extends Component {
+  dataChanged = this.dataChanged.bind(this);
+
+  dataChanged(data) {
+    let id = Object.keys(data)[0];
+    let editedItem = data[Object.keys(data)];
+
+    this.props.onEdit(id, editedItem, "todos");
   }
-  return (
-    <div className="row my-4">
-      <div className="col-md-2" />
-      <div className="col-md-8 todos">
-        <h1 className="h5">Todo Items</h1>
-        <ul className="list-group">{todoItems}</ul>
+
+  customValidateText(text) {
+    return text.length > 0 && text.length < 64;
+  }
+
+  render() {
+    let todoItems;
+    if (this.props.todos) {
+      todoItems = this.props.todos.map((todo, i) => {
+        return (
+          <li
+            key={i}
+            className="list-group-item list-group-item-action text-left"
+          >
+            <InlineEdit
+              validate={this.customValidateText}
+              activeClassName="editing"
+              text={todo.todoItem}
+              paramName={todo.id}
+              change={this.dataChanged}
+            />
+            <div className="btn-toolbar float-right">
+              <button
+                className="btn btn-success done"
+                title="Done"
+                onClick={this.props.onMoveItem.bind(this, todo, "todos")}
+              >
+                <i className="fa fa-check-square-o" aria-hidden="true" />
+              </button>
+              <button
+                className="btn btn-danger delete"
+                title="Delete"
+                onClick={this.props.onDelete.bind(this, todo.id, "todos")}
+              >
+                <i className="fa fa-trash-o" aria-hidden="true" />
+              </button>
+            </div>
+          </li>
+        );
+      });
+    }
+    return (
+      <div className="row my-4">
+        <div className="col-md-2" />
+        <div className="col-md-8 todos">
+          <h1 className="h5">Todo Items</h1>
+          <ul className="list-group">{todoItems}</ul>
+        </div>
+        <div className="col-md-2" />
       </div>
-      <div className="col-md-2" />
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Todos;
