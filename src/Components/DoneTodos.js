ae5cf811bc18bbe89f7b2795901f06d7db44c1f3
jayrexacilo <acilojayrex@gmail.com>
2017-11-20 19:02:05 +0800

    Added Update button and replaced button icons with font-awesome

--- src/Components/DoneTodos.js
+++ src/Components/DoneTodos.js
@@ -1,32 +1,46 @@
-import React from 'react';
+import React from "react";
 
-const DoneTodos = (props) => {
-    let doneTodoItems;
-    if(props.doneTodos) {
-        doneTodoItems = props.doneTodos.map((doneTodo, i) => {
-            return (
-                <li key={i} className="list-group-item list-group-item-action text-left">
-                    {doneTodo.todoItem}
-                    <div className="btn-toolbar float-right">
-                        <button className="btn btn-success done" onClick={props.onMoveItem.bind(this, doneTodo, 'doneTodos')}>✓</button>
-                        <button className="btn btn-danger delete" onClick={props.onDelete.bind(this, doneTodo.id, 'doneTodos')}>✗</button>
-                    </div>
-                </li>
-            );
-        });
-    }
-    return (
-        <div className="row">
-            <div className="col-md-2"></div>
-            <div className="col-md-8 doneTodos">
-                <h1 className="h5">Done Items</h1>
-                <ul className="list-group">
-                    {doneTodoItems}
-                </ul>
-            </div>
-            <div className="col-md-2"></div>
-        </div>
-    );
+const DoneTodos = props => {
+  let doneTodoItems;
+  if (props.doneTodos) {
+    doneTodoItems = props.doneTodos.map((doneTodo, i) => {
+      return (
+        <li
+          key={i}
+          className="list-group-item list-group-item-action text-left"
+        >
+          {doneTodo.todoItem}
+          <div className="btn-toolbar float-right">
+            <button className="btn btn-light update">
+              <i className="fa fa-pencil-square-o" aria-hidden="true" />
+            </button>
+            <button
+              className="btn btn-success done"
+              onClick={props.onMoveItem.bind(this, doneTodo, "doneTodos")}
+            >
+              <i className="fa fa-check-square-o" aria-hidden="true" />
+            </button>
+            <button
+              className="btn btn-danger delete"
+              onClick={props.onDelete.bind(this, doneTodo.id, "doneTodos")}
+            >
+              <i className="fa fa-trash-o" aria-hidden="true" />
+            </button>
+          </div>
+        </li>
+      );
+    });
+  }
+  return (
+    <div className="row">
+      <div className="col-md-2" />
+      <div className="col-md-8 doneTodos">
+        <h1 className="h5">Done Items</h1>
+        <ul className="list-group">{doneTodoItems}</ul>
+      </div>
+      <div className="col-md-2" />
+    </div>
+  );
 };
 
 export default DoneTodos;
