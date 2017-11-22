94d63d683f0c62c7c265096923c8f5f12a80a385
jayrexacilo <acilojayrex@gmail.com>
2017-11-22 15:00:13 +0800

    Used the express-promise-router from async await method

--- api/controllers/todos.js
+++ api/controllers/todos.js
@@ -2,20 +2,12 @@ const Todo = require("../models/todo");
 
 module.exports = {
   index: async (req, res, next) => {
-    try {
-      const todos = await Todo.find({});
-      res.status(200).json(todos);
-    } catch (err) {
-      next(err);
-    }
+    const todos = await Todo.find({});
+    res.status(200).json(todos);
   },
   newTodo: async (req, res, next) => {
-    try {
-      const newTodo = new Todo(req.body);
-      const todo = await newTodo.save();
-      res.status(201).json(todo);
-    } catch (err) {
-      next(err);
-    }
+    const newTodo = new Todo(req.body);
+    const todo = await newTodo.save();
+    res.status(201).json(todo);
   }
 };
