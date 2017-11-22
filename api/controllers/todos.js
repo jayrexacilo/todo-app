716848b6f360fb123306fdb46bb6972e47c4ddd9
jayrexacilo <acilojayrex@gmail.com>
2017-11-22 14:55:21 +0800

    Refactored for education purposes(from a callback to promise request)

--- api/controllers/todos.js
+++ api/controllers/todos.js
@@ -1,24 +1,21 @@
 const Todo = require("../models/todo");
 
 module.exports = {
-  index: (req, res, next) => {
-    Todo.find({})
-      .then(todos => {
-        res.status(200).json(todos);
-      })
-      .catch(err => {
-        next(err);
-      });
+  index: async (req, res, next) => {
+    try {
+      const todos = await Todo.find({});
+      res.status(200).json(todos);
+    } catch (err) {
+      next(err);
+    }
   },
-  newTodo: (req, res, next) => {
-    const newTodo = new Todo(req.body);
-    newTodo
-      .save()
-      .then(todos => {
-        res.status(201).json(todos);
-      })
-      .catch(err => {
-        next(err);
-      });
+  newTodo: async (req, res, next) => {
+    try {
+      const newTodo = new Todo(req.body);
+      const todo = await newTodo.save();
+      res.status(201).json(todo);
+    } catch (err) {
+      next(err);
+    }
   }
 };
