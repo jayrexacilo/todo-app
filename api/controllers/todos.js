e4cb5bc7b8c6233e868c5acfe33188e21d75745a
jayrexacilo <acilojayrex@gmail.com>
2017-11-22 15:16:44 +0800

    Used the express-promise-router dependency from async await method

--- api/controllers/todos.js
+++ api/controllers/todos.js
@@ -1,6 +1,7 @@
 const Todo = require("../models/todo");
 
 module.exports = {
+<<<<<<< HEAD
   index: async (req, res, next) => {
     const todos = await Todo.find({});
     res.status(200).json(todos);
@@ -9,5 +10,26 @@ module.exports = {
     const newTodo = new Todo(req.body);
     const todo = await newTodo.save();
     res.status(201).json(todo);
+=======
+  index: (req, res, next) => {
+    Todo.find({})
+      .then(todos => {
+        res.status(200).json(todos);
+      })
+      .catch(err => {
+        next(err);
+      });
+  },
+  newTodo: (req, res, next) => {
+    const newTodo = new Todo(req.body);
+    newTodo
+      .save()
+      .then(todos => {
+        res.status(201).json(todos);
+      })
+      .catch(err => {
+        next(err);
+      });
+>>>>>>> 9b40f49... Created a todos API with POST and GET request
   }
 };
--- api/routes/todos.js
+++ api/routes/todos.js
@@ -1,6 +1,10 @@
 const express = require("express");
+<<<<<<< HEAD
 // const router = express.Router();
 const router = require("express-promise-router")();
+=======
+const router = express.Router();
+>>>>>>> 9b40f49... Created a todos API with POST and GET request
 
 const TodosController = require("../controllers/todos");
 
