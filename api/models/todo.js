56aa5c956ba8fdff5434463aab3ca8d113488770
jayrexacilo <acilojayrex@gmail.com>
2017-11-23 13:39:36 +0800

    Changed todo Schema object keys&props

--- api/models/todo.js
+++ api/models/todo.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const todoSchema = new Schema({
-  id: String,
-  todoItem: String
+  todoName: String,
+  done: Boolean
 });
 
 const Todo = mongoose.model("todo", todoSchema);
