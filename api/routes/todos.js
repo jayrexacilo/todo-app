124456931f6857f1e1b90c7c6ff3dde6cfb5f47a
jayrexacilo <acilojayrex@gmail.com>
2017-11-24 15:03:31 +0800

    added param validation for delete route

--- api/routes/todos.js
+++ api/routes/todos.js
@@ -4,6 +4,8 @@ const router = require("express-promise-router")();
 
 const TodosController = require("../controllers/todos");
 
+const { validateParam, schemas } = require("../helpers/routeHelpers");
+
 router
   .route("/")
   .get(TodosController.index)
@@ -13,6 +15,11 @@ router.route("/todo").get(TodosController.getTodo);
 router.route("/done").get(TodosController.getDoneTodo);
 router.route("/put").put(TodosController.replaceTodo);
 router.route("/patch/").patch(TodosController.editTodo);
-router.route("/delete/:todoID").delete(TodosController.deleteTodo);
+router
+  .route("/delete/:todoID")
+  .delete(
+    validateParam(schemas.idSchema, "todoID"),
+    TodosController.deleteTodo
+  );
 
 module.exports = router;
