9842c1a238f1ad6bc551b0ccd8ddffa85d4c70e4
jayrexacilo <acilojayrex@gmail.com>
2017-12-07 00:48:07 +0800

    testing connect DB

--- api/app.js
+++ api/app.js
@@ -5,7 +5,7 @@ const bodyParser = require("body-parser");
 
 mongoose.Promise = global.Promise;
 mongoose.connect(
-  "mongodb://todo-app-kaizen:todoAppKaizen@cluster0-shard-00-00-gtwrw.mongodb.net:27017,cluster0-shard-00-01-gtwrw.mongodb.net:27017,cluster0-shard-00-02-gtwrw.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin"
+  "mongodb://todoDB:todoAppKaizen@ds033196.mlab.com:33196/todo-api"
 );
 
 const app = express();
